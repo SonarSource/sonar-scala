129da96ea1a274db860cd33f503d85778e356b8e
Lucien Bart <lucien.bart@sonarsource.com>
2025-12-11 15:40:20 +0100

    SONARSCALA-35 Add nested test and expected error message

--- sonar-scala-plugin/src/test/resources/checks/StringLiteralDuplicatedScala.scala
+++ sonar-scala-plugin/src/test/resources/checks/StringLiteralDuplicatedScala.scala
@@ -2,7 +2,7 @@ class MyClass {
   def foo(str: String) = {
     str match {
       case "Alice" =>
-        val message = "Hello dear" + "Alice" // Noncompliant
+        val message = "Hello dear" + "Alice" // Noncompliant {{Define a constant instead of duplicating this literal "Hello dear" 3 times.}}
         message
       case "Bob" =>
         val message = "Hello dear" + "Bob"
@@ -38,24 +38,48 @@ class MyClass {
   }
 
 
-  object Compliant {
+  object CompliantAnnotation {
     @SuppressWarnings("scala:S1192") // Compliant
-    def a(): String =  "Hello2!" + "Hello2!" + "Hello2!" // Noncompliant
+    def a(): String =  "Hello2!" + "Hello2!" + "Hello2!" // Noncompliant {{Define a constant instead of duplicating this literal "Hello2!" 3 times.}}
     @SuppressWarnings("scala:S1192")
-    def b(): String =  "Welcome2!" + "Welcome2!" + "Welcome2!" // Noncompliant
+    def b(): String =  "Welcome2!" + "Welcome2!" + "Welcome2!" // Noncompliant {{Define a constant instead of duplicating this literal "Welcome2!" 3 times.}}
     @SuppressWarnings("scala:S1192")
-    def c(): String = "ByeBye!" +  "ByeBye!" + "ByeBye!" // Noncompliant
+    def c(): String = "ByeBye!" +  "ByeBye!" + "ByeBye!" // Noncompliant {{Define a constant instead of duplicating this literal "ByeBye!" 3 times.}}
   }
-  object Compliant2 {
+  object CompliantAnnotation2 {
     @SuppressWarnings("scala:S1192") // Compliant
-    def a(): String = "scala:S1192" + "scala:S1192" + "scala:S1192" // Noncompliant
+    def a(): String = "scala:S1192" + "scala:S1192" + "scala:S1192" // Noncompliant {{Define a constant instead of duplicating this literal "scala:S1192" 3 times.}}
+  }
+
+  object CompliantAnnotation3 {
+    @Deprecated(since = "6582.23.1354") // Compliant
+    def a(): String = "6582.23.1354" + "6582.23.1354" + "6582.23.1354" // Noncompliant {{Define a constant instead of duplicating this literal "6582.23.1354" 3 times.}}
   }
 
   @SuppressWarnings("scala:S1193") // Compliant
   object Reproducer {
     @SuppressWarnings("scala:S1193")
-    def a(): String =  "Hello!" + "Hello!" + "Hello!" // Noncompliant
+    def a(): String =  "Hello!" + "Hello!" + "Hello!" // Noncompliant {{Define a constant instead of duplicating this literal "Hello!" 3 times.}}
     @SuppressWarnings("scala:S1193")
-    def b(): String =  "Welcome!" + "Welcome!" + "Welcome!" // Noncompliant
+    def b(): String =  "Welcome!" + "Welcome!" + "Welcome!" // Noncompliant {{Define a constant instead of duplicating this literal "Welcome!" 3 times.}}
   }
+
+  @Deprecated(since = "2051.30.21654") // Compliant
+  object Reproducer2 {
+    @Deprecated(since = "2051.30.21654")
+    def a(): String =  ""
+    @Deprecated(since = "2051.30.21654")
+    def b(): String =  ""
+  }
+
+  class MyNestedLiteral(values: Array[String]) extends scala.annotation.StaticAnnotation
+
+  @MyNestedLiteral(Array("some-nested-string")) // Compliant
+  object Reproducer3 {
+    @MyNestedLiteral(Array("some-nested-string"))
+    def a(): String =  ""
+    @MyNestedLiteral(Array("some-nested-string", "some-nested-string", "some-nested-string"))
+    def b(): String =  ""
+  }
+
 }
