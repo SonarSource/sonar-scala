3f1e74242fcdce14320356149802facc5919bd65
Tomasz Tylenda <tomasz.tylenda@sonarsource.com>
2026-05-15 10:25:31 +0200

    Add fail() to validate CI

--- its/plugin/src/test/java/org/sonarsource/slang/SonarLintIntegrationTest.java
+++ its/plugin/src/test/java/org/sonarsource/slang/SonarLintIntegrationTest.java
@@ -46,6 +46,7 @@ import org.sonarsource.sonarlint.core.test.utils.junit5.SonarLintTestHarness;
 import org.sonarsource.sonarlint.core.test.utils.plugins.Plugin;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import static org.assertj.core.api.Assertions.tuple;
 import static org.awaitility.Awaitility.await;
 
@@ -126,6 +127,8 @@ public class SonarLintIntegrationTest {
       .extracting(RaisedIssueDto::getRuleKey, RaisedIssueDto::getTextRange)
       .usingRecursiveFieldByFieldElementComparator()
       .containsExactly(tuple("scala:S1145", new TextRangeDto(4, 8, 4, 12)));
+
+    fail("To Validate CI behaviour (new Junit5 test)");
   }
 
   private List<RaisedIssueDto> analyzeFileAndGetIssues(URI fileUri) {
